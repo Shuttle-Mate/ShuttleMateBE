f5bdf623bc78bf90f4a72b0cdb772f253576a48a
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2025-05-28 12:09:59 +0700

    update

--- ShuttleMate.API/Program.cs
+++ ShuttleMate.API/Program.cs
@@ -16,12 +16,11 @@ builder.Services.AddConfig(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-    
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
 app.UseHttpsRedirection();
 app.UseAuthentication();
 
