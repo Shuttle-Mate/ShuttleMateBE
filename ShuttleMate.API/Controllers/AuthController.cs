9d5e4fd9507ce59d3ad3227f3470815ae1397f78
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2025-05-28 11:46:20 +0700

    update

--- ShuttleMate.API/Controllers/AuthController.cs
+++ ShuttleMate.API/Controllers/AuthController.cs
@@ -13,7 +13,6 @@ namespace ShuttleMate.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
-
         public AuthController(IAuthService authService)
         {
             _authService = authService;
