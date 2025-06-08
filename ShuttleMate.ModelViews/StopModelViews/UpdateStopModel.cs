2ebd168415d94b948b2fa74d0777d389e3fd6f84
dofu18 <phu2003hk@gmail.com>
2025-06-08 22:43:12 +0700

    stop temp

---
+++ ShuttleMate.ModelViews/StopModelViews/UpdateStopModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuttleMate.ModelViews.StopModelViews
+{
+    public class UpdateStopModel
+    {
+        public Guid RouteId { get; set; }
+        public string Name { get; set; }
+        public string Ward { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+        public int StopOrder { get; set; }
+    }
+}
