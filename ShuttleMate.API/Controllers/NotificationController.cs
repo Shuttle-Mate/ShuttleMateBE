c89befd69654033d4e3b43dff3c079c13cc00f49
dofu18 <phu2003hk@gmail.com>
2025-06-09 01:28:03 +0700

    noti basic api

---
+++ ShuttleMate.API/Controllers/NotificationController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShuttleMate.Contract.Services.Interfaces;
+using ShuttleMate.Core.Bases;
+using ShuttleMate.Core.Constants;
+using ShuttleMate.ModelViews.NotificationModelViews;
+using ShuttleMate.ModelViews.ShuttleModelViews;
+using ShuttleMate.Services.Services;
+
+namespace ShuttleMate.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNoti(NotiModel model)
+        {
+            await _notificationService.CreateNotification(model);
+            return Ok(new BaseResponseModel<string>(
+                statusCode: StatusCodes.Status200OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: "Tạo thông báo thành công!"
+            ));
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllNoti()
+        {
+            var res = await _notificationService.GetAll();
+            return Ok(new BaseResponseModel<List<ResponseNotiModel>>(
+                statusCode: StatusCodes.Status200OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: res
+            ));
+        }
+        [HttpGet("{notiId}")]
+        public async Task<IActionResult> GetNotiById(Guid notiId)
+        {
+            var res = await _notificationService.GetById(notiId);
+            return Ok(new BaseResponseModel<ResponseNotiModel>(
+                statusCode: StatusCodes.Status200OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: res
+            ));
+        }
+        //[HttpPatch]
+        //public async Task<IActionResult> UpdateShuttle(UpdateShuttleModel model)
+        //{
+        //    await _shuttleService.UpdateShuttle(model);
+        //    return Ok(new BaseResponseModel<string>(
+        //        statusCode: StatusCodes.Status200OK,
+        //        code: ResponseCodeConstants.SUCCESS,
+        //        data: "Cập nhật xe thành công"
+        //    ));
+        //}
+        [HttpDelete]
+        public async Task<IActionResult> DeleteNotification(Guid notiId)
+        {
+            await _notificationService.DeleteNoti(notiId);
+            return Ok(new BaseResponseModel<string>(
+                statusCode: StatusCodes.Status200OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: "Xóa thông báo thành công"
+            ));
+        }
+    }
+}
