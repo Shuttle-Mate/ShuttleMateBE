2427e9ca45b919cbc1e947fcbef03c50c3fe9bac
truongdaivy57 <truongdaivy57@gmail.com>
2025-07-08 15:55:44 +0700

    fix: add field to promotion

--- ShuttleMate.Contract.Repositories/Entities/Promotion.cs
+++ ShuttleMate.Contract.Repositories/Entities/Promotion.cs
@@ -13,8 +13,9 @@ namespace ShuttleMate.Contract.Repositories.Entities
     public class Promotion : BaseEntity
     {
         public string Description { get; set; }
-        public decimal? DiscountPrice { get; set; }
-        public decimal? DiscountPercent { get; set; }
+        public decimal? DiscountPrice { get; set; } //DIRECT_DISCOUNT
+        public decimal? DiscountPercent { get; set; } //PERCENTAGE_DISCOUNT
+        public decimal? DiscountAmount { get; set; } //FIXED_AMOUNT_DISCOUNT
         public decimal? LimitSalePrice { get; set; }
         public DateTime EndDate { get; set; }
         public int UsingLimit { get; set; } = 0;
