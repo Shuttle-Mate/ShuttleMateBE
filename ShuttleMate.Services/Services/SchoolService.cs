f544347d449abe970f756274f73c0a855475ce62
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2025-07-12 13:00:41 +0700

    add update school api

--- ShuttleMate.Services/Services/SchoolService.cs
+++ ShuttleMate.Services/Services/SchoolService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using ShuttleMate.Contract.Repositories.Entities;
 using ShuttleMate.Contract.Repositories.IUOW;
 using ShuttleMate.Contract.Services.Interfaces;
@@ -47,6 +48,29 @@ namespace ShuttleMate.Services.Services
                 LastUpdatedTime = DateTime.Now,
             };
             await _unitOfWork.GetRepository<School>().InsertAsync(school);
+            await _unitOfWork.SaveAsync();
+        }
+        public async Task UpdateSchool(UpdateSchoolModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Name))
+            {
+                throw new ErrorException(StatusCodes.Status400BadRequest, ErrorCode.BadRequest, "Tên trường không được để trống!");
+            }
+            if (string.IsNullOrWhiteSpace(model.Address))
+            {
+                throw new ErrorException(StatusCodes.Status400BadRequest, ErrorCode.BadRequest, "Địa chỉ trường không được để trống!");
+            }
+            if (string.IsNullOrWhiteSpace(model.SchoolTime.ToString()))
+            {
+                throw new ErrorException(StatusCodes.Status400BadRequest, ErrorCode.BadRequest, "Thời gian trường không được để trống!");
+            }
+           var school =await _unitOfWork.GetRepository<School>().Entities.FirstOrDefaultAsync(x=>x.Id == model.Id)?? throw new ErrorException(StatusCodes.Status404NotFound, ErrorCode.NotFound, "Không tìm thấy trường!");
+
+            school.Name = model.Name;
+            school.Address = model.Address;
+            school.SchoolTime = model.SchoolTime;
+
+            await _unitOfWork.GetRepository<School>().UpdateAsync(school);
             await _unitOfWork.SaveAsync();
         }
     }
