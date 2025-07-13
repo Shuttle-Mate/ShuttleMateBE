d48f00e5700a0ae0e234f2a0585f2535c0a31d76
dofu18 <phu2003hk@gmail.com>
2025-07-13 19:45:56 +0700

    temp

--- ShuttleMate.Contract.Services/Interfaces/IRouteStopService.cs
+++ ShuttleMate.Contract.Services/Interfaces/IRouteStopService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShuttleMate.ModelViews.ShuttleModelViews;
 using ShuttleMate.ModelViews.StopModelViews;
 
 namespace ShuttleMate.Contract.Services.Interfaces
@@ -10,5 +11,7 @@ namespace ShuttleMate.Contract.Services.Interfaces
     public interface IRouteStopService
     {
         Task AssignStopsToRouteAsync(AssignStopsToRouteModel model);
+        Task<List<ResponseShuttleModel>> GetAll();
+
     }
 }
