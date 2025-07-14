9b59ffb2598cc808eb394f6e6a9e8875b2cb556b
truongdaivy57 <truongdaivy57@gmail.com>
2025-07-14 15:50:53 +0700

    feat: add Ward entity and update dayabase

--- ShuttleMate.Contract.Repositories/Entities/Stop.cs
+++ ShuttleMate.Contract.Repositories/Entities/Stop.cs
@@ -10,9 +10,11 @@ namespace ShuttleMate.Contract.Repositories.Entities
     public class Stop : BaseEntity
     {
         public string Name { get; set; }
-        public string Ward {  get; set; }
-        public decimal Lat {  get; set; }
-        public decimal Lng { get; set; }
+        public double Lat {  get; set; }
+        public double Lng { get; set; }
+        public string RefId { get; set; }
+        public Guid WardId { get; set; }
+        public virtual Ward Ward { get; set; }
         public virtual ICollection<StopEstimate> StopEstimates { get; set; } = new List<StopEstimate>();
         public virtual ICollection<RouteStop> RouteStops { get; set; } = new List<RouteStop>();
     }
---
+++ ShuttleMate.Contract.Repositories/Entities/Ward.cs
@@ -0,0 +1,10 @@
+﻿using ShuttleMate.Contract.Repositories.Base;
+
+namespace ShuttleMate.Contract.Repositories.Entities
+{
+    public class Ward : BaseEntity
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Stop> Stops { get; set; } = new List<Stop>();
+    }
+}
