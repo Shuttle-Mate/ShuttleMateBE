35ef408d5452ec3c7040ad3fc8c9abbd16fa0bde
truongdaivy57 <truongdaivy57@gmail.com>
2025-07-28 21:02:47 +0700

    fix: create and update with estimate time

--- ShuttleMate.Contract.Services/Interfaces/IScheduleService.cs
+++ ShuttleMate.Contract.Services/Interfaces/IScheduleService.cs
@@ -8,6 +8,6 @@ namespace ShuttleMate.Contract.Services.Interfaces
         Task<BasePaginatedList<ResponseScheduleModel>> GetAllByRouteIdAsync(Guid routeId, string? direction, bool sortAsc, int page = 0, int pageSize = 10);
         Task<IEnumerable<ResponseTodayScheduleForDriverModel>> GetAllTodayAsync();
         Task CreateAsync(CreateScheduleModel model);
-        Task UpdateAsync(UpdateScheduleModel model);
+        Task UpdateAsync(Guid scheduleId, UpdateScheduleModel model);
     }
 }
