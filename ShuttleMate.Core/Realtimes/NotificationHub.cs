60600100a7e3d1053b0ae6fcc438283df4d02828
dofu18 <phu2003hk@gmail.com>
2025-07-29 23:09:56 +0700

    push notification test

---
+++ ShuttleMate.Core/Realtimes/NotificationHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using ShuttleMate.Core.Bases;
+using ShuttleMate.Core.Constants;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuttleMate.Core.Realtimes
+{
+    public class NotificationHub : Hub
+    {
+        private readonly ILogger<NotificationHub> _logger;
+
+        public NotificationHub(ILogger<NotificationHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task SendNotificationToUser(string userId, string message)
+        {
+            await Clients.User(userId).SendAsync("ReceiveNotification", message);
+        }
+    }
+}
---
+++ ShuttleMate.ModelViews/NotificationModelViews/NotificationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuttleMate.ModelViews.NotificationModelViews
+{
+    public class NotificationRequest
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string DeviceToken { get; set; }
+    }
+}
