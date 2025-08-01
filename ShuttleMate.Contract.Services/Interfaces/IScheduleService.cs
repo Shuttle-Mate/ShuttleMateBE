d667d0d542c4b320824e983b90538d1a9cc36b85
truongdaivy57 <truongdaivy57@gmail.com>
2025-08-01 21:40:05 +0700

    feat: get by id and delete

--- ShuttleMate.Contract.Services/Interfaces/IScheduleService.cs
+++ ShuttleMate.Contract.Services/Interfaces/IScheduleService.cs
@@ -7,7 +7,9 @@ namespace ShuttleMate.Contract.Services.Interfaces
     {
         Task<BasePaginatedList<ResponseScheduleModel>> GetAllByRouteIdAsync(Guid routeId, string? direction, bool sortAsc, int page = 0, int pageSize = 10);
         Task<IEnumerable<ResponseTodayScheduleForDriverModel>> GetAllTodayAsync();
+        Task<ResponseScheduleModel> GetByIdAsync(Guid scheduleId);
         Task CreateAsync(CreateScheduleModel model);
         Task UpdateAsync(Guid scheduleId, UpdateScheduleModel model);
+        Task DeleteAsync(Guid scheduleId);
     }
 }
