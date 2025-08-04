a6a3f58dbbe81a55c139a8c06dc0184c809ccc58
Mai Nguyen Long Vu <156437514+mainguyenlongvu@users.noreply.github.com>
2025-08-04 13:56:04 +0700

    Update getinfor and get by id

--- ShuttleMate.ModelViews/UserModelViews/ChildResponse.cs
+++ ShuttleMate.ModelViews/UserModelViews/ChildResponse.cs
@@ -10,14 +10,15 @@ namespace ShuttleMate.ModelViews.UserModelViews
     public class ChildResponse
     {
         public Guid Id { get; set; }
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string ProfileImageUrl { get; set; }
+        public string? Email { get; set; }
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ProfileImageUrl { get; set; }
         public DateTime? DateOfBirth { get; set; }
         //public int Balance { get; set; }
         public string? Address { get; set; }
         public bool Gender { get; set; }
+        public SchoolResponse? School { get; set; }
 
     }
 }
--- ShuttleMate.ModelViews/UserModelViews/UserInforModel.cs
+++ ShuttleMate.ModelViews/UserModelViews/UserInforModel.cs
@@ -10,17 +10,17 @@ namespace ShuttleMate.ModelViews.UserModelViews
     public class UserInforModel
     {
         public Guid Id { get; set; }
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string ProfileImageUrl { get; set; }
+        public string? Email { get; set; }
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ProfileImageUrl { get; set; }
         public DateTime? DateOfBirth { get; set; }
         //public int Balance { get; set; }
         public string? Address { get; set; }
         public bool Gender { get; set; }
         public ParentResponse? Parent { get; set; }
-        public List<ChildResponse> Childs { get; set; }
-        public SchoolResponse School { get; set; }
-        public string RoleName { get; set; }
+        public List<ChildResponse>? Childs { get; set; }
+        public SchoolResponse? School { get; set; }
+        public string? RoleName { get; set; }
     }
 }
