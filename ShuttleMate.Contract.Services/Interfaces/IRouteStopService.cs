a6f086e157bf5fd02276dc4e04b866932595af90
truongdaivy57 <truongdaivy57@gmail.com>
2025-08-06 11:06:03 +0700

    fix: add more field in driver schedule and fix search stop with route

--- ShuttleMate.Contract.Services/Interfaces/IRouteStopService.cs
+++ ShuttleMate.Contract.Services/Interfaces/IRouteStopService.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ShuttleMate.Core.Bases;
+﻿using ShuttleMate.Core.Bases;
 using ShuttleMate.ModelViews.RouteStopModelViews;
-using ShuttleMate.ModelViews.ShuttleModelViews;
 using ShuttleMate.ModelViews.StopModelViews;
 
 namespace ShuttleMate.Contract.Services.Interfaces
@@ -13,7 +7,6 @@ namespace ShuttleMate.Contract.Services.Interfaces
     public interface IRouteStopService
     {
         Task AssignStopsToRouteAsync(AssignStopsToRouteModel model);
-        //Task<List<ResponseShuttleModel>> GetAll();
-        Task<BasePaginatedList<StopWithRouteResponseModel>> SearchStopWithRoutes(double lat, double lng, GetRouteStopQuery req);
+        Task<BasePaginatedList<StopWithRouteResponseModel>> SearchStopWithRoutes(double lat, double lng, Guid schoolId, int page = 0, int pageSize = 10);
     }
 }
