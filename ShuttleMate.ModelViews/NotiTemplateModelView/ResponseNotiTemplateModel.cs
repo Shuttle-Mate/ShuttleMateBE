1ed3066d76713ffce6f85cfa4c62dbb6157e4b80
dofu18 <phu2003hk@gmail.com>
2025-08-14 15:37:53 +0700

    noti template api

---
+++ ShuttleMate.ModelViews/NotiTemplateModelView/ResponseNotiTemplateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuttleMate.ModelViews.NotiTemplateModelView
+{
+    public class ResponseNotiTemplateModel
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Template { get; set; }
+    }
+}
