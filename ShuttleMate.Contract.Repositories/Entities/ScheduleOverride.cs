27a5d2276f2b83afc4ecc1891f64c0d5d5f972a7
truongdaivy57 <truongdaivy57@gmail.com>
2025-08-18 13:49:20 +0700

    fix(database): separate reason into 2

--- ShuttleMate.Contract.Repositories/Entities/ScheduleOverride.cs
+++ ShuttleMate.Contract.Repositories/Entities/ScheduleOverride.cs
@@ -5,16 +5,17 @@ namespace ShuttleMate.Contract.Repositories.Entities
     public class ScheduleOverride : BaseEntity
     {
         public DateOnly Date { get; set; }
-        public string? Reason { get; set; }
         public Guid ScheduleId { get; set; }
         public virtual Schedule Schedule { get; set; }
         public Guid OriginalShuttleId { get; set; }
         public virtual Shuttle OriginalShuttle { get; set; }
         public Guid? OverrideShuttleId { get; set; }
+        public string? Reason { get; set; }
         public virtual Shuttle? OverrideShuttle { get; set; }
         public Guid OriginalUserId { get; set; }
         public virtual User OriginalUser { get; set; }
         public Guid? OverrideUserId { get; set; }
         public virtual User? OverrideUser { get; set; }
+        public string? DriverReason { get; set; }
     }
 }
