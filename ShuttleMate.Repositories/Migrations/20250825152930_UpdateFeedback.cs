f67f73b5029db2d816e69d43dce8b0c00540bb18
truongdaivy57 <truongdaivy57@gmail.com>
2025-08-25 22:29:56 +0700

    fix: update feedback

---
+++ ShuttleMate.Repositories/Migrations/20250825152930_UpdateFeedback.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShuttleMate.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Feedbacks_TripId",
+                table: "Feedbacks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_TripId",
+                table: "Feedbacks",
+                column: "TripId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Feedbacks_TripId",
+                table: "Feedbacks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_TripId",
+                table: "Feedbacks",
+                column: "TripId",
+                unique: true);
+        }
+    }
+}
